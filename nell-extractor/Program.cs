971232a68c72bf51be296b95b25b3eeb28583f0c
Mark Garnett <thecommunistduck@hotmail.co.uk>
2015-11-01 22:17:17 +0000

    Initial commit.

---
+++ nell-extractor/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace nell_extractor
+{
+    class Entry
+    {
+        public string Entity;
+        public string Category;
+        public string OtherCategories;
+        public string Name;
+    }
+
+    class EntryComparer : IEqualityComparer<Entry>
+    {
+
+        public bool Equals(Entry x, Entry y)
+        {
+           return x.Entity == y.Entity;
+        }
+
+        public int GetHashCode(Entry obj)
+        {
+            return obj.Entity.GetHashCode();
+        }
+    }
+
+    static class Extractor
+    {
+        public static string conceptString = "";
+        public static string concept = "concept:food";
+        static bool generalised = false;
+        static void Main(string[] args)
+        {
+            conceptString = args[0];
+            generalised = (args[1] == "general");
+            concept = "concept:" + conceptString;
+            ConcurrentBag<Entry> entries = new ConcurrentBag<Entry>();
+
+            Parallel.ForEach(File.ReadLines("nell.csv"), (line, _, lineNumber) =>
+            {
+                string[] split = line.Split('\t').Select(t => t.Trim()).ToArray();
+                if (split[1] != "generalizations" || !IsRelatedToConcept(split))
+                    return;
+                else
+                    entries.Add(GenerateEntry(split));
+            });
+            HashSet<Entry> cleanedEntries = new HashSet<Entry>(entries, new EntryComparer());
+            using(StreamWriter writer = new StreamWriter(conceptString+".txt"))
+            {
+                foreach (Entry e in cleanedEntries)
+                    writer.WriteLine(e.Entity + "\t" + e.Category + " " + e.OtherCategories + "\t" + e.Name);
+            }
+
+            Console.WriteLine("Found a total of {0} entries, written to {1}.", cleanedEntries.Count, conceptString+".txt");
+            Console.ReadLine();
+        }
+
+        public static bool IsRelatedToConcept(string[] columns)
+        {
+            return (columns[2] == concept || columns[10].Contains(concept) || (generalised && columns[11].Contains(concept)));
+            //if you want 'indirect' things - e.g. condiments listed as concept:condiment, as condiment is an instance of food - use this as well.
+            //  || columns[11].Contains(concept));
+        }
+
+        public static Entry GenerateEntry(string[] columns)
+        {
+            return new Entry()
+            {
+                Entity = columns[0].Split(':').Last(),
+                Category = columns[2].Split(':').Last(),
+                OtherCategories = String.Join(" ", (columns[10]).Split(' ').Select(t => t.Split(':').Last()).Where(c => c != columns[2].Split(':').Last())),
+                Name = columns[8]
+            };
+        }
+    }
+}
---
+++ nell-extractor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("nell-extractor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("nell-extractor")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("82516639-92c8-496c-83a5-b834c1c1fe29")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
